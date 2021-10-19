29390155aa7ee233cdf4a501f762591b74549f82
Mukhil <76261800+Mukhil04@users.noreply.github.com>
2021-10-19 00:09:04 -0500

    Delete mp3-code-2 directory

--- mp3-code-2/run_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
-
-if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('tests')
-    JSONTestRunner(visibility='after_published').run(suite)
-    # students shouldn't be able to see results until grades are published
-    #JSONTestRunner(visibility='after_due_date').run(suite)
